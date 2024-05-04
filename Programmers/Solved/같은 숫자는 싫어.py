628dcc2dd0a2dffb2db23e595e6c1b4c20c091bd
0Hooni <thddudgns972@gmail.com>
2024-05-04 14:05:14 +0900

    Update 같은 숫자는 싫어.py

--- Programmers/Solved/같은 숫자는 싫어.py
+++ Programmers/Solved/같은 숫자는 싫어.py
@@ -1,9 +1,11 @@
 def solution(arr):
     answer = []
-    prev_i = None
-    for i in range(0, len(arr)):
-        
+    answer.append(arr[0])
 
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i - 1]:
+            continue
+        else:
+            answer.append(arr[i])
 
-    
     return answer

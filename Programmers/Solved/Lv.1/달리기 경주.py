19149cd3f493302053a9b7a56d5b8ad9ab3576d6
0Hooni <thddudgns972@gmail.com>
2024-06-14 23:20:51 +0900

    Update 달리기 경주.py

--- Programmers/Solved/Lv.1/달리기 경주.py
+++ Programmers/Solved/Lv.1/달리기 경주.py
@@ -1,10 +1,10 @@
-def solution(n, m, section):
-    answer = 0
-    painted = 0
+def solution(players, callings):
+    player_dict = {player: idx for idx, player in enumerate(players)}
 
-    for wall in section:
-        if painted < wall:
-            painted = wall + m - 1
-            answer += 1
+    for name in callings:
+        idx = player_dict[name]
+        players[idx], players[idx - 1] = players[idx - 1], players[idx]
+        player_dict[players[idx]] = idx
+        player_dict[players[idx - 1]] = idx - 1
 
-    return answer
+    return players

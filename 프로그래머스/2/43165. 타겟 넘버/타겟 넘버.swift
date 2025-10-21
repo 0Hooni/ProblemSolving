831206b645e4750ffeb8461a8d277936bf802b6d
YeongHoon Song <thddudgns972@gmail.com>
2025-10-21 18:57:34 +0900

    [level 2] Title: 타겟 넘버, Time: 1507.65 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -2,23 +2,29 @@ import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     var result = 0
-
-    func dfs(_ numbers: [Int], _ sum: Int, _ isMinus: Bool) {
-        var numbers = numbers
-        let cur = numbers.removeFirst()
-        var sum = sum + (isMinus ? -cur : cur)
-
-        // 마지막 노드에 오면 sum을 비교
-        if numbers.isEmpty {
-            if sum == target { result += 1 }
-        } else {    // 마지막이 아니면 (+, -)로 나눠짐
-            dfs(numbers, sum, true)
-            dfs(numbers, sum, false)
-        }
-    }
     
-    dfs(numbers, 0, true)
-    dfs(numbers, 0, false)
+    dfs(numbers, target, 0, true, &result)
+    dfs(numbers, target, 0, false, &result)
     
     return result
+}
+
+func dfs(
+    _ numbers: [Int], 
+    _ target: Int, 
+    _ sum: Int, 
+    _ isMinus: Bool, 
+    _ result: inout Int
+) {
+    var numbers = numbers
+    let cur = numbers.removeFirst()
+    var sum = sum + (isMinus ? -cur : cur)
+
+    // 마지막 노드에 오면 sum을 비교
+    if numbers.isEmpty {
+        if sum == target { result += 1 }
+    } else {    // 마지막이 아니면 (+, -)로 나눠짐
+        dfs(numbers, target, sum, true, &result)
+        dfs(numbers, target, sum, false, &result)
+    }
 }

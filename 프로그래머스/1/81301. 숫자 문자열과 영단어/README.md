77dc33a1cfa4b35cc2812026e41da22b88e426ff
YeongHoon Song <thddudgns972@gmail.com>
2025-10-21 15:52:17 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.38 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.11 ms
+메모리: 16.6 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 21일 15:47:00
+2025년 10월 21일 15:52:15
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.swift
@@ -1,23 +1,34 @@
 import Foundation
 
+// func solution(_ s:String) -> Int {
+//     var stack: [String] = []
+//     var result: [String] = []
+    
+//     let nums: [String: Int] = ["zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9]
+    
+//     for ch in s {
+//         let str = String(ch)
+//         if "0" <= ch && ch <= "9" { result.append(str) }
+//         else {
+//             stack.append(str)
+//             let curStr = String(stack.joined())
+//             if let curNum = nums[curStr] {
+//                 result.append(String(curNum))
+//                 stack.removeAll()
+//             }
+//         }
+//     }
+    
+//     return Int(result.joined())!
+// }
+
 func solution(_ s:String) -> Int {
-    var stack: [String] = []
-    var result: [String] = []
+    var s = s
+    let nums = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
-    let nums: [String: Int] = ["zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9]
-    
-    for ch in s {
-        let str = String(ch)
-        if "0" <= ch && ch <= "9" { result.append(str) }
-        else {
-            stack.append(str)
-            let curStr = String(stack.joined())
-            if let curNum = nums[curStr] {
-                result.append(String(curNum))
-                stack.removeAll()
-            }
-        }
+    for (i, num) in nums.enumerated() {
+        s = s.replacingOccurrences(of: num, with: String(i))
     }
     
-    return Int(result.joined())!
+    return Int(s)!
 }

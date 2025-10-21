aa5b6a5793eedd61ae99058dfd3e6d36ab810f62
YeongHoon Song <thddudgns972@gmail.com>
2025-10-21 16:05:26 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 7.34 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -1,11 +1,7 @@
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    // n번째 index의 문자를 추출
-    // 해당 문자를 정렬 -> 어디 출신인지 알아야됨
-    // 해당 출신지로 다시 정렬 결과 반환
-    
     return strings.sorted {
-        let str1 = $0[$0.index($0.startIndex, offsetBy: n)]
-        let str2 = $1[$1.index($1.startIndex, offsetBy: n)]
+        let str1 = Array($0)[n]
+        let str2 = Array($1)[n]
         if str1 == str2 { return $0 < $1 }
         else { return str1 < str2 }
     }

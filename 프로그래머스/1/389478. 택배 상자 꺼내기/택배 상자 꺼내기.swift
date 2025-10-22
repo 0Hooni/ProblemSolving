d2133aaf628745e144daaf2068a80f6088f5ae3d
YeongHoon Song <thddudgns972@gmail.com>
2025-10-22 17:36:06 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.swift
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.swift
@@ -2,14 +2,11 @@ import Foundation
 
 func solution(_ maxNum:Int, _ width:Int, _ target:Int) -> Int {
     let targetPos = findPosition(width, target)
-    print(targetPos)
     
     // 2. 계산 결과 x가 같고 y가 더 위에 있다면 count++
     var count = 1
-    
     for n in 1...maxNum {
         let curPos = findPosition(width, n)
-        print("box:", n, "위치:", curPos.x, curPos.y)
         if curPos.x == targetPos.x && curPos.y > targetPos.y { 
             count += 1 
         }
@@ -22,12 +19,16 @@ struct Position {
     let x: Int
     let y: Int
 }
+// 
 
 /// 1. 상자의 개수와 가로폭을 기준으로 특정 n에 대한 좌표를 계산할 수학적 논리를 작성
 func findPosition(_ width: Int, _ target: Int) -> Position {
-    let n = target - 1
-    return Position(
-        x: ((n % (2 * width)) < width) ? (n % width) : (((2 * width) - (n % (2 * width)) - 1) % width), 
-        y: n / width
-    )
+    let idx = target - 1
+    let row = idx / width
+    let col = idx % width
+
+    // 짝수 행: 왼 -> 오
+    // 홀수 행: 오 -> 왼
+    let x = (row % 2 == 0) ? col : (width - 1 - col)
+    return Position(x: x, y: row)
 }

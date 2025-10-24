1ed340fb564a81d248c7ad6fe51c1fdab0da2f05
YeongHoon Song <thddudgns972@gmail.com>
2025-10-24 20:58:26 +0900

    [level 2] Title: 괄호 변환, Time: 12.76 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+func solution(_ p:String) -> String {
+    let result = dfs(p)
+    
+    return result
+}
+
+func dfs(_ w: String) -> String {
+    if w == "" { return "" }
+    
+    let devided = devideBalandedString(w)
+    var u = devided[0]
+    let v = devided[1]
+    
+    if isRightString(u) { return u + dfs(v) }
+    else {
+        var empty = "(" + dfs(v) + ")"
+        u.removeFirst()
+        u.removeLast()
+        u = swap(u)
+        empty += u
+        return empty
+    }
+}
+
+func swap(_ p: String) -> String {
+    var stack: [String] = []
+    let p = Array(p).map { String($0) }
+    
+    for _p in p {
+        if _p == ")" { stack.append("(") }
+        else { stack.append(")") }
+    }
+    
+    return stack.joined()
+}
+
+func isRightString(_ p: String) -> Bool {
+    var stack: [String] = []
+    
+    for str in p {
+        if str == "(" { stack.append(String(str)) }
+        else {
+            if stack.isEmpty { return false }
+            else { stack.removeLast() }
+        }
+    }
+    
+    return stack.isEmpty
+}
+
+func devideBalandedString(_ p: String) -> [String] {
+    let p = Array(p).map { String($0) }
+    
+    var openStr = 0
+    var closedStr = 0
+    for (idx, str) in p.enumerated() {
+        if str == "(" { openStr += 1}
+        else { closedStr += 1}
+        
+        if openStr == closedStr {
+            return [
+                p[0...idx].map { String($0) }.joined(), 
+                p[(idx+1)..<p.count].map { String($0) }.joined()
+            ]
+        }
+    }
+    
+    return []
+}

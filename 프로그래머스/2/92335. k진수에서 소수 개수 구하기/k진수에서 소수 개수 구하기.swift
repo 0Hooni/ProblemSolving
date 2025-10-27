68188e1e3ed1c3f2807d6e4f4eb0028a426330a4
YeongHun Song <thddudgns972@gmail.com>
2025-10-28 00:31:42 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 13.65 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+func solution(_ n:Int, _ k:Int) -> Int {
+    // 0 기준 스플릿 -> 필요없는 인수 제거(0, 1) -> 소수 판별
+    // 해당 N 은 K 진법에서도 10진법에서도 소수여야 함.
+    let kStr = trans(n, k)
+    let numStrArr = kStr.components(separatedBy: "0").filter { 
+        $0 != "" && $0 != "0" && $0 != "1"
+    }
+    
+    // 필요시 소수 결과를 저장한 primeDict 만들기
+    var prime: [Int: Bool] = [:]
+    
+    var count = 0
+    for num in numStrArr {
+        if isKPrime(num, k, &prime) { count += 1 }
+    }
+    
+    return count
+}
+
+func isKPrime(
+    _ num: String, 
+    _ k: Int, 
+    _ prime: inout [Int: Bool]
+) -> Bool {
+    let num = Int(num)!
+    
+    if let isPrime = prime[num] { return isPrime } 
+    else {
+        let res = isPrime(num)
+        prime[num] = res
+        return res
+    }
+    
+    return false
+}
+
+func isPrime(_ n: Int) -> Bool {
+    if n == 2 || n == 3 { return true }
+    
+    for i in 2...Int(sqrt(Float(n))) {
+        if n % i == 0 { return false }
+    }
+    
+    return true
+}
+
+func trans(_ n: Int, _ k: Int) -> String {
+    var res = ""
+    var n = n
+    while n > 0 {
+        res += String(n % k)
+        n = n / k
+    }
+    
+    return String(res.reversed())
+}

eee213e91809a217e5b2cee387ff30b57c4c6f9f
YeongHun Song <thddudgns972@gmail.com>
2025-10-27 23:41:18 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 361.03 ms, Memory: 46.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
+    var q1 = queue1.map { Int64($0) }
+    var q2 = queue2.map { Int64($0) }
+    let total = (q1.reduce(Int64(0), +) + q2.reduce(Int64(0), +))
+    
+    if total % 2 != 0 { return -1 }
+    let target = total / 2
+    
+    if max(q1.max() ?? 0, q2.max() ?? 0) > target { return -1 }
+    
+    let arr = q1 + q2
+    let N = arr.count
+    
+    var i = 0, j = q1.count
+    var sum1 = q1.reduce(Int64(0), +)
+    var moves = 0
+    let limit = 2 * N + 5
+    
+    while moves <= limit {
+        if sum1 == target { return moves }
+        
+        if sum1 > target {
+            let x = arr[i % N]
+            sum1 -= x
+            i += 1
+        } else {
+            let x = arr[j % N]
+            sum1 += x
+            j += 1
+        }
+        
+        moves += 1
+    }
+    
+    return -1
+}
